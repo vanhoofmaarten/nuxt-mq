0c3ef7004f2e323277da6bb7288cfac673534d80
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2020-12-20 17:33:03 +0100

    test: use fetch for static tests

--- test/module.test.js
+++ test/module.test.js
@@ -4,26 +4,25 @@ const {
 } = require('jest-dev-server')
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
+const fetch = require('node-fetch')
 const port = 3000
 const url = path => `http://localhost:${port}${path}`
 const timeout = 30 * 1000
 
-const setup = async config => {
+const setup = async ({ config, staticMode }) => {
   await exec(`nuxt generate test/fixture -c ${config}`)
+  const command = staticMode
+    ? `http-server test/fixture/dist --port=${port}`
+    : `nuxt start test/fixture -c ${config} --port=${port}`
   await setupDevServer({
-    command: `nuxt start test/fixture -c ${config} --port=${port}`,
+    command,
     launchTimeout: timeout,
     port
   })
 }
 
-const getConfigName = ({ mode, mq } = { mq: true }) => [
-  'nuxt',
-  'config',
-  ...mq ? ['mq'] : [],
-  mode.toLowerCase(),
-  'js'
-].join('.')
+const getConfigName = ({ mode, mq } = { mq: true }) =>
+  ['nuxt', 'config', ...(mq ? ['mq'] : []), mode.toLowerCase(), 'js'].join('.')
 
 describe.each(['SSR', 'Static'])('%s', mode => {
   afterEach(async () => {
@@ -33,7 +32,8 @@ describe.each(['SSR', 'Static'])('%s', mode => {
   test(
     'default',
     async () => {
-      await setup(getConfigName({ mode }))
+      const config = getConfigName({ mq: true, mode })
+      await setup({ config, staticMode: false })
       await page.goto(url('/'))
       const html = await page.content()
       expect(html).toContain('Works!')
@@ -44,11 +44,10 @@ describe.each(['SSR', 'Static'])('%s', mode => {
   test(
     'with mq',
     async () => {
-      const configName = getConfigName({ mq: true, mode })
-      await setup(configName)
-      const nuxtWithMqConfig = require(`./fixture/${configName}`)
-      await page.goto(url('/mq'))
-      const html = await page.content()
+      const config = getConfigName({ mq: true, mode })
+      await setup({ config, staticMode: true })
+      const nuxtWithMqConfig = require(`./fixture/${config}`)
+      const html = await fetch(url('/mq')).then(res => res.text())
       expect(html).toContain(nuxtWithMqConfig.mq.defaultBreakpoint)
     },
     timeout
